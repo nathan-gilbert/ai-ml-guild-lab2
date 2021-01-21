33c2e136765635aa46012323122d33d6b6c777fe
Nathan Gilbert <nathan.gilbert@gmail.com>
2021-01-20 20:07:27 -0700

    updating script

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
 import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
 from perceptron.perceptron import Perceptron
 
 
+def show_results(ops, weights):
+    for op in ops:
+        final_result = Perceptron.sigma(weights[0] + op[0]*weights[1] + op[1]*weights[2])
+        print(f"{weights[0]} + {op[0]}*{weights[1]} + {op[1]}*{weights[2]} = {final_result}")
+
+
 if __name__ == '__main__':
-    # AND Gate
-    X = np.array([
+    # Binary Op
+    binary_ops = np.array([
         [0, 0],
         [0, 1],
         [1, 0],
         [1, 1]
     ])
-    and_gate_labels = np.array([0, 0, 0, 1])
-    #or_gate_labels = np.array([0, 1, 1, 1])
 
     perceptron = Perceptron(input_size=2)
-    perceptron.train(X, and_gate_labels)
-    print("Weight vector: ", perceptron.W)
+    and_labels = np.array([0, 0, 0, 1])
+    perceptron.train(binary_ops, and_labels)
+    print("1st Weight vector: ", perceptron.W)
+
+    show_results(binary_ops, perceptron.W)
+    or_labels = np.array([0, 1, 1, 1])
+    perceptron.train(binary_ops, or_labels)
+    print("2nd Weight vector: ", perceptron.W)
+    show_results(binary_ops, perceptron.W)
+
+    xor_labels = np.array([0, 1, 1, 0])
+    perceptron.train(binary_ops, xor_labels)
+    print("3rd Weight vector: ", perceptron.W)
+    show_results(binary_ops, perceptron.W)
+
+    # Now try some real data
+    # Load the data set
+    bc = datasets.load_breast_cancer()
+    X = bc.data
+    y = bc.target
+
+    # Create training and test split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
+
+    perceptron = Perceptron(input_size=len(X_train))
+    perceptron.train(X_train, y_train)
--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class Perceptron:
+class Perceptron(object):
     """Implements a single layer Perceptron """
 
     def __init__(self, input_size, epochs=100, lr=1):
@@ -16,10 +16,22 @@ class Perceptron:
         # training set but is supplied.
         self.lr = lr
 
-    def sigma(self, x):
+    @staticmethod
+    def sigma(x):
+        """This is the activation function. """
         return 1 if x >= 0 else 0
 
+    def train(self, X, d):
+        """Iterate through the training data (X) and score the perceptrons
+        performance via the labels (d) and adjust weights accordingly."""
+        for _ in range(self.epochs):
+            for i in range(d.shape[0]):
+                y = self.predict(X[i])
+                e = d[i] - y
+                self.W = self.W + self.lr * e * np.insert(X[i], 0, 1)
+
     def predict(self, x):
+        """y = sigma(z) """
         # add the bias term
         x = np.insert(x, 0, 1)
         # W^T * x (inner product)
@@ -28,12 +40,3 @@ class Perceptron:
         a = self.sigma(z)
         # return predicted label
         return a
-
-    def train(self, X, d):
-        """Iterate through the training data and score the perceptrons
-        performance, adjust weights accordingly."""
-        for _ in range(self.epochs):
-            for i in range(d.shape[0]):
-                y = self.predict(X[i])
-                e = d[i] - y
-                self.W = self.W + self.lr * e * np.insert(X[i], 0, 1)
